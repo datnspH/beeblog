d1b2fb2a5c296e7a2944715517ad4adb071a884a
datnspH <johndoe@example.comdatnsph25191@fpt.edu.vn>
2022-11-10 13:50:06 +0700

    first commit

---
+++ lab1.php
@@ -0,0 +1,26 @@
+<?php
+$name="dat";
+$agre=40;
+$id="hp25191";
+$a=array(2,4,6);
+$tr=array(
+    'name'=>"dat",
+    'id' =>"ph25911",
+);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Thông tin sinh viên</h2>
+    <p>Họ và tên: <?php echo $tr['name'] ?></p>
+    <p>tuổi</p>
+    <p>mã sinh viên</p>
+    
+</body>
+</html>
